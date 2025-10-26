876024c8afba93d59c026fb672d7de516de5c917
kiteudikisnedekhi-bb <jsbca202@gmail.com>
2025-10-27 03:09:20 +0530

    added manual headers for CORS in catalog

--- src/catalog/catalog.controller.ts
+++ src/catalog/catalog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Header, Param, Query } from '@nestjs/common';
 import { CatalogService } from './catalog.service';
 
 @Controller('catalog')
@@ -6,11 +6,13 @@ export class CatalogController {
   constructor(private readonly catalogService: CatalogService) {}
 
   @Get('products')
+  @Header('Access-Control-Allow-Origin', '*')
   async getProducts() {
     return this.catalogService.getProducts();
   }
 
   @Get('products/:id')
+  @Header('Access-Control-Allow-Origin', '*')
   async getProduct(@Param('id') id: number, @Query('distance') distance: number, @Query('floor') floor: number) {
     return this.catalogService.getProduct(id, distance, floor);
   }
