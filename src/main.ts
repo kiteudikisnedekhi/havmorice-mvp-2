ad34a7878c945aadc6ffc989f2a42b9f53198f4e
kiteudikisnedekhi-bb <jsbca202@gmail.com>
2025-10-27 02:59:43 +0530

    disabled frontend caching

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,8 @@ dotenv.config();  // Load .env file
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://havmorice-mvp-frontend-2-production.up.railway.app',
+    origin: true,  // Allow all origins for testing
     credentials: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://havmorice-mvp-frontend-2-production.up.railway.app');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-      return;
-    }
-    next();
   });
   await app.listen(3000);
 }
