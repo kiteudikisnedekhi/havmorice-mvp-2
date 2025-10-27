c274e8c52ef88105e7595b52e330c98dcd3ab137
kiteudikisnedekhi-bb <jsbca202@gmail.com>
2025-10-27 20:58:43 +0530

    Attempt to enforce global CORS

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import * as dotenv from 'dotenv';
 
-dotenv.config();  // Load .env file
+  dotenv.config();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    credentials: true,
-  });
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-  await app.listen(3000);
-}
-bootstrap();
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: '*',
+      credentials: true,
+    });
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Credentials', 'true');
+      res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      next();
+    });
+    await app.listen(process.env.PORT || 3000);
+  }
+  bootstrap();
